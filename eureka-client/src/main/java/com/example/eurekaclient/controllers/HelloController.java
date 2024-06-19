94f954c64c5b5e48ca2fa6a4febd83702b75b79f
Shreya Kumari <157014499+shreya-arya00@users.noreply.github.com>
2024-06-19 16:41:35 +0530

    Add files via upload

---
+++ eureka-client/src/main/java/com/example/eurekaclient/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.eurekaclient.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Eureka Client!";
+    }
+}
